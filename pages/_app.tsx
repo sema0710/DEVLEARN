3f09e00a8d17475b99ee44bb7c1f6af799aa6099
ohjunsang <pandati@dsm.hs.kr>
2020-12-07 20:01:38 +0900

    [CREATE] disable provider and add globalstyle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import { Provider } from 'react-redux';
 import store from '../src/data/store';
+import GlobalStyle from '../src/style/GlobalStyle';
 
 export default class RootApp extends App {
 	render() {
 		const { Component, ...other } = this.props;
 		return (
-			<Container>
-				<Provider store={store}>
-					<Component {...other}></Component>
-				</Provider>
-			</Container>
+			<>
+				<GlobalStyle />
+				{/* <Provider store={store}> */}
+				<Component {...other}></Component>
+				{/* </Provider> */}
+			</>
 		);
 	}
 }
