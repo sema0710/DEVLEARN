e538bef37597dad2d8d6265e93327212333bb1fc
ohjunsang <pandati@dsm.hs.kr>
2020-12-07 20:01:20 +0900

    [CREATE] signin page

---
+++ pages/signin.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import React from 'react';
+import SignInContainer from '../src/container/signin';
+
+const SignIn: FC = () => {
+	return <SignInContainer />;
+};
+
+export default SignIn;
