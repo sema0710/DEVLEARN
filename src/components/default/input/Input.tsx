ba8c10714a675a457c2c0e6e60da8df5b0e83d33
ohjunsang <pandati@dsm.hs.kr>
2020-12-07 19:58:45 +0900

    [ADD] input add props

--- src/components/default/input/Input.tsx
+++ src/components/default/input/Input.tsx
@@ -2,16 +2,30 @@ import React, { FC } from 'react';
 import * as S from './style';
 
 interface Props {
-	width: number;
-	height: number;
+	width: string;
+	height: string;
 	setValue: (value: string) => void;
+	placeholder?: string;
+	type?: string;
+	fontSize?: string;
+	margin?: string;
 }
 
-const Input: FC<Props> = ({ width, height, setValue }) => {
+const Input: FC<Props> = ({ width, height, setValue, placeholder, type, fontSize, margin }) => {
 	const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setValue(event.target.value);
 	};
-	return <S.Input height={height} width={width} onChange={inputChangeHandler} />;
+	return (
+		<S.Input
+			height={height}
+			width={width}
+			onChange={inputChangeHandler}
+			placeholder={placeholder}
+			type={type}
+			fontSize={fontSize}
+			margin={margin}
+		/>
+	);
 };
 
 export default Input;
