a4a387558d9958e525ec50b06cd4feaabf61c1a6
ohjunsang <pandati@dsm.hs.kr>
2020-12-07 20:00:10 +0900

    [CREATE] signin

---
+++ src/components/signIn/signIn.tsx
@@ -0,0 +1,29 @@
+import { Input, Button } from 'components/default';
+import React, { FC } from 'react';
+import * as S from './style';
+
+const ID_PLACE_HOLDER = '아이디';
+const PASSWORD_PLACE_HOLDER = '비밀번호';
+
+const SignIn: FC = () => {
+	const setValue = value => {
+		console.log(value);
+	};
+	return (
+		<S.SignIn>
+			<S.SignInBody>
+				<S.SignInTitle>LOGIN</S.SignInTitle>
+				<Input {...S.InputStyle} setValue={setValue} placeholder={ID_PLACE_HOLDER} />
+				<Input
+					{...S.InputStyle}
+					setValue={setValue}
+					placeholder={PASSWORD_PLACE_HOLDER}
+					type='password'
+				/>
+				<Button {...S.ButtonStyle}>로그인</Button>
+			</S.SignInBody>
+		</S.SignIn>
+	);
+};
+
+export default SignIn;
