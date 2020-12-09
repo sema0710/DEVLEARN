fe4e3855da8b92f682f7419a486cf65e12c6812f
ohjunsang <pandati@dsm.hs.kr>
2020-12-09 15:15:10 +0900

    [ADD] title add margin props

--- src/components/default/title/title.tsx
+++ src/components/default/title/title.tsx
@@ -1,8 +1,12 @@
 import React, { FC } from 'react';
 import * as S from './style';
 
-const Title: FC = ({ children }) => {
-	return <S.Title>{children}</S.Title>;
+interface Props {
+	margin?: string;
+}
+
+const Title: FC<Props> = ({ children, margin }) => {
+	return <S.Title margin={margin}>{children}</S.Title>;
 };
 
 export default Title;
