4481930227327315061689796a5ef56ceb0b879b
ohjunsang <pandati@dsm.hs.kr>
2020-12-09 15:15:50 +0900

    [ADD] add buttonClickHandlers

--- src/components/signUp/imgSettingPage/imgSettingPage.tsx
+++ src/components/signUp/imgSettingPage/imgSettingPage.tsx
@@ -1,15 +1,23 @@
 import { SubTitle, Title, Button } from 'components/default';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import * as S from '../style';
 import ImgInput from './imgInput';
 
-const ImgSettingPage: FC = () => {
+interface Props {
+	pageRef: React.RefObject<HTMLDivElement>;
+	buttonClickHandler: () => void;
+	setImg: (file: File) => void;
+}
+
+const ImgSettingPage: FC<Props> = ({ pageRef, buttonClickHandler, setImg }) => {
 	return (
-		<S.SignUpBody>
+		<S.SignUpBody ref={pageRef}>
 			<Title>SET PROFILE IMG</Title>
 			<ImgInput />
 			<SubTitle margin='20px'>사진을 등록해 주세요.</SubTitle>
-			<Button {...S.ButtonStyle}>다음으로</Button>
+			<Button {...S.ButtonStyle} onClick={buttonClickHandler}>
+				다음으로
+			</Button>
 		</S.SignUpBody>
 	);
 };
