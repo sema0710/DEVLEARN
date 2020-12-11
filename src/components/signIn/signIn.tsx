d1df3713fc77bef07bc12aa031a228a9c0002565
ohjunsang <pandati@dsm.hs.kr>
2020-12-11 13:35:00 +0900

    [FIX] error

--- src/components/signIn/signIn.tsx
+++ src/components/signIn/signIn.tsx
@@ -7,26 +7,26 @@ const ID_PLACE_HOLDER = '아이디';
 const PASSWORD_PLACE_HOLDER = '비밀번호';
 
 const SignIn: FC = () => {
-	const setValue = value => {
-		console.log(value);
-	};
-	return (
-		<S.SignIn>
-			<S.SignInBody>
-				<Title>LOGIN</Title>
-				<Input {...S.InputStyle} setValue={setValue} placeholder={ID_PLACE_HOLDER} />
-				<Input
-					{...S.InputStyle}
-					setValue={setValue}
-					placeholder={PASSWORD_PLACE_HOLDER}
-					type='password'
-				/>
-				<TextButton />
-				<S.SignInTextButton />
-				<Button {...S.ButtonStyle}>로그인</Button>
-			</S.SignInBody>
-		</S.SignIn>
-	);
+  const setValue = value => {
+    console.log(value);
+  };
+  return (
+    <S.SignIn>
+      <S.SignInBody>
+        <Title>LOGIN</Title>
+        <Input {...S.InputStyle} setValue={setValue} placeholder={ID_PLACE_HOLDER} />
+        <Input
+          {...S.InputStyle}
+          setValue={setValue}
+          placeholder={PASSWORD_PLACE_HOLDER}
+          type='password'
+        />
+        <TextButton />
+        <S.SignInTextButton />
+        <Button {...S.ButtonStyle}>로그인</Button>
+      </S.SignInBody>
+    </S.SignIn>
+  );
 };
 
 export default SignIn;
