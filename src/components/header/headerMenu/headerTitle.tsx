a5e965cadfdf9cbae7be100cd6bdc52334d43fb5
ohjunsang <pandati@dsm.hs.kr>
2020-12-11 13:56:04 +0900

    [CREATE] headerTitle

---
+++ src/components/header/headerMenu/headerTitle.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import * as S from '../style';
+
+const HeaderTitle: FC = () => {
+  return <S.HeaderTitle>DEVLEARN</S.HeaderTitle>;
+};
+
+export default HeaderTitle;
