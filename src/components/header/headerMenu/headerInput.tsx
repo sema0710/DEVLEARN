09aec785082ed81a897b29ee8979ec1febf133e2
ohjunsang <pandati@dsm.hs.kr>
2020-12-11 13:36:26 +0900

    [CREATE] header input

---
+++ src/components/header/headerMenu/headerInput.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import * as S from '../style';
+
+const HeaderInput: FC = () => {
+  return (
+    <S.HeaderSearchInputWrapper>
+      <S.HeaderSearchInput />
+      <div />
+    </S.HeaderSearchInputWrapper>
+  );
+};
+
+export default HeaderInput;
