0f45d097a0b6be378a6c2439d53a1f56726fdb52
ohjunsang <pandati@dsm.hs.kr>
2020-12-14 22:02:53 +0900

    [FIX] naming fix

--- src/framwork/module/redux/action/post/interface/index.ts
+++ src/framwork/module/redux/action/post/interface/index.ts
@@ -1,4 +1,4 @@
-import { IGetPosts, GET_POSTS } from './IPostAction';
+import { IGetPosts, GET_POSTS } from './post';
 export type IPostAction = IGetPosts;
 export type { IGetPosts };
 export { GET_POSTS };
