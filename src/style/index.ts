36c3d714ea7692bc4bb02be15905fc8302661aee
ohjunsang <pandati@dsm.hs.kr>
2020-12-14 23:33:12 +0900

    [ADD] add style color

--- src/style/index.ts
+++ src/style/index.ts
@@ -1,12 +1,14 @@
 export const color = {
-	main: '#4B4B4B',
-	sub: '#6C6C6C',
-	font: '#FFFFFF',
-	header: '#3B3B3B',
+  main: '#4B4B4B',
+  sub: '#6C6C6C',
+  font: '#FFFFFF',
+  header: '#3B3B3B',
+  success: '#57FFAB',
+  fail: '#FFA5A5',
 };
 
 const HTML_FONT_SIZE = 16;
 
 export const pxToRem = (px: number): number => {
-	return px / HTML_FONT_SIZE;
+  return px / HTML_FONT_SIZE;
 };
