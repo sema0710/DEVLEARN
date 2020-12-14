f7c3d870c3f8bb51d313b267c19f20d9394c6469
ohjunsang <pandati@dsm.hs.kr>
2020-12-14 20:22:07 +0900

    [FIX] add redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import App from 'next/app';
 import { Provider } from 'react-redux';
-import store from '../src/data/store';
+import store from '../src/framwork/module/redux/store';
 import GlobalStyle from '../src/style/GlobalStyle';
 
 export default class RootApp extends App {
-	render() {
-		const { Component, ...other } = this.props;
-		return (
-			<>
-				<GlobalStyle />
-				{/* <Provider store={store}> */}
-				<Component {...other}></Component>
-				{/* </Provider> */}
-			</>
-		);
-	}
+  render() {
+    const { Component, ...other } = this.props;
+    return (
+      <>
+        <GlobalStyle />
+        <Provider store={store}>
+          <Component {...other}></Component>
+        </Provider>
+      </>
+    );
+  }
 }
