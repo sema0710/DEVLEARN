0b10f70aeefe91c93120635e486c4043d03793c1
ohjunsang <pandati@dsm.hs.kr>
2020-12-14 20:27:37 +0900

    [MOVE] move post style

--- src/framwork/components/post/style/index.ts
+++ src/framwork/components/post/style/index.ts
@@ -5,24 +5,37 @@ export const PostElement = styled.div`
   width: 100%;
   color: ${color.font};
   border-bottom: 1px solid ${color.font};
+  padding: ${pxToRem(10)}rem ${pxToRem(40)}rem;
+  box-sizing: border-box;
+  margin: ${pxToRem(20)}rem 0px;
 `;
 
 export const PostElementTitle = styled.h1`
   font-weight: 100;
-  font-size: ${pxToRem(30)}rem;
-  text-decoration: underline;
+  font-size: ${pxToRem(35)}rem;
+  display: inline-block;
+  border-bottom: 1px solid rgba(0, 0, 0, 0);
+  transition: all 0.2s;
+  cursor: pointer;
+  &:hover {
+    border-bottom: 1px solid white;
+    box-sizing: border-box;
+  }
 `;
 
 export const PostElementTagWrapper = styled.div`
   display: flex;
+  height: ${pxToRem(20)}rem;
+  margin-bottom: 10px;
 `;
 
 export const PostElementTag = styled.div`
   font-weight: 100;
   font-size: ${pxToRem(15)}rem;
+  cursor: pointer;
 `;
 
 export const PostElementDescription = styled.div`
   font-weight: 100;
-  font-size: ${pxToRem(20)}rem;
+  font-size: ${pxToRem(25)}rem;
 `;
