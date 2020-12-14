3401de8c1f93de8d8dd88c9ef139b175b422ff23
ohjunsang <pandati@dsm.hs.kr>
2020-12-14 23:31:08 +0900

    [ADD] PostEntity add field

--- src/domain/entity/post/PostEntity.ts
+++ src/domain/entity/post/PostEntity.ts
@@ -1,14 +1,16 @@
 import IPostEntity from './interface/IPostEntity';
 
 class PostEntity implements IPostEntity {
-  constructor(title: string, description: string, tags: string[]) {
+  constructor(title: string, description: string, tags: string[], isResolved: boolean) {
     this.title = title;
     this.description = description;
     this.tags = tags;
+    this.isResolved = isResolved;
   }
   title: string;
   description: string;
   tags: string[];
+  isResolved: boolean;
 }
 
 export default PostEntity;
