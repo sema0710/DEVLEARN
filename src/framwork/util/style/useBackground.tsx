2c85e2f936e54c1d99f0584ca00d682a7a2e7b9a
ohjunsang <pandati@dsm.hs.kr>
2020-12-15 18:58:40 +0900

    [DELETE] 쓸모없다고 판단 후 삭제

--- src/framwork/util/style/useBackground.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Background from '../../components/default/background';
-
-const useBackground = (component: React.ReactNode) => {
-  return <Background>{component}</Background>;
-};
-
-export default useBackground;
