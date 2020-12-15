d848ea692d656e84f464a0b5e5956215dd03e6ec
ohjunsang <pandati@dsm.hs.kr>
2020-12-15 18:39:20 +0900

    [CREATE] background component

---
+++ src/framwork/components/default/background/Background.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import * as S from './style';
+
+const Background: FC = ({ children }) => {
+  return <S.Background>{children}</S.Background>;
+};
+
+export default Background;
