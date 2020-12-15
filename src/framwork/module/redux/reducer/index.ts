19d4dc6047d6d5fd90cfae0143f96ecba1b31c72
ohjunsang <pandati@dsm.hs.kr>
2020-12-15 18:55:35 +0900

    [ADD] combinereducer

--- src/framwork/module/redux/reducer/index.ts
+++ src/framwork/module/redux/reducer/index.ts
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import PostReducer from './post';
+import UserReducer from './user';
 
-export default combineReducers({ PostReducer });
+export default combineReducers({ PostReducer, UserReducer });
