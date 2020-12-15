0d53a4724f372c5b823553d2124d909af6609237
ohjunsang <pandati@dsm.hs.kr>
2020-12-15 18:48:40 +0900

    [FIX] add useHeader and useBackground

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,20 @@ import App from 'next/app';
 import { Provider } from 'react-redux';
 import store from '../src/framwork/module/redux/store';
 import GlobalStyle from '../src/style/GlobalStyle';
-import UseHeader from '../src/framwork/util/header';
+import { useHeader, useBackground } from '../src/framwork/util/style';
 
 export default class RootApp extends App {
   render() {
     const { Component, ...other } = this.props;
-    return (
-      <>
-        <GlobalStyle />
-        <Provider store={store}>
-          <UseHeader>
+    return useBackground(
+      useHeader(
+        <>
+          <GlobalStyle />
+          <Provider store={store}>
             <Component {...other} />
-          </UseHeader>
-        </Provider>
-      </>
+          </Provider>
+        </>,
+      ),
     );
   }
 }
