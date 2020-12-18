5d3f238c561126cd2035b04867ffc4cd169940fe
ohjunsang <pandati@dsm.hs.kr>
2020-12-18 11:57:57 +0900

    [CREATE] write page

---
+++ pages/write.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import WritePostContainer from '../src/framwork/container/writePost';
+
+const Write = () => {
+  return <WritePostContainer />;
+};
+
+export default Write;
