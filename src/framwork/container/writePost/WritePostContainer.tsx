17f25bb7488393fd9120a97d170f5938a22fe8e5
ohjunsang <pandati@dsm.hs.kr>
2020-12-18 12:00:40 +0900

    [CREATE] WritePostContainer

---
+++ src/framwork/container/writePost/WritePostContainer.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+import WritePost from '../../components/writePost';
+const WritePostContainer: FC = () => {
+  return <WritePost />;
+};
+
+export default WritePostContainer;
