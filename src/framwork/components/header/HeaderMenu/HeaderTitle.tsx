9dca5bc6ad0cb691445ff83c4d0a65ced0c13c15
ohjunsang <pandati@dsm.hs.kr>
2021-01-07 09:46:52 +0900

    [ADD] add function when click header logo, go to main page

--- src/framwork/components/header/HeaderMenu/HeaderTitle.tsx
+++ src/framwork/components/header/HeaderMenu/HeaderTitle.tsx
@@ -1,8 +1,13 @@
 import React, { FC } from 'react';
+import Link from 'next/link';
 import * as S from '../style';
 
 const HeaderTitle: FC = () => {
-  return <S.HeaderTitle>DEVLEARN</S.HeaderTitle>;
+  return (
+    <S.HeaderTitle>
+      <Link href='/'>DEVLEARN</Link>
+    </S.HeaderTitle>
+  );
 };
 
 export default HeaderTitle;
