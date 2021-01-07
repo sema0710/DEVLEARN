06577411e1b441b277eec6ee3dabb28d257d1777
ohjunsang <pandati@dsm.hs.kr>
2021-01-07 09:50:09 +0900

    [ADD] add post interface

--- src/framwork/module/redux/action/post/interface/IPostAction.ts
+++ src/framwork/module/redux/action/post/interface/IPostAction.ts
@@ -4,6 +4,8 @@ export const GET_MY_POSTS = 'POST/GET_MY_POSTS' as const;
 export const GET_POSTS = 'POST/GET_POSTS' as const;
 export const SET_POSTS = 'POST/SET_POSTS' as const;
 export const SET_MY_POSTS = 'POST/SET_MY_POSTS' as const;
+export const GET_DETAIL_POST = 'POST/GET_DETAIL_POST' as const;
+export const SET_DETAIL_POST = 'POST/SET_DETAIL_POST' as const;
 
 export interface IGetPosts {
   type: typeof GET_POSTS;
@@ -23,4 +25,20 @@ export interface ISetMyPost {
   payload: IPostEntity[];
 }
 
-export type IPostAction = IGetPosts | ISetPost | IGetMyPosts | ISetMyPost;
+export interface IGetDetailPost {
+  type: typeof GET_DETAIL_POST;
+  payload: string;
+}
+
+export interface ISetDetailPost {
+  type: typeof SET_DETAIL_POST;
+  payload: IPostEntity;
+}
+
+export type IPostAction =
+  | IGetPosts
+  | ISetPost
+  | IGetMyPosts
+  | ISetMyPost
+  | IGetDetailPost
+  | ISetDetailPost;
