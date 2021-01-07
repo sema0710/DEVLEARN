bfe6beeb7cb408a6816ef7f4e0fd02c18c2cf0e4
ohjunsang <pandati@dsm.hs.kr>
2021-01-07 09:51:45 +0900

    [FIX] post reducer interface

--- src/framwork/module/redux/reducer/post/interface/IPostReducer.ts
+++ src/framwork/module/redux/reducer/post/interface/IPostReducer.ts
@@ -4,6 +4,8 @@ import { IPostAction } from '../../../action/post/interface';
 export interface IPostState {
   post: PostEntity[];
   mypost: PostEntity[];
+  detailPost: PostEntity;
+  answer: PostEntity[];
 }
 
 export interface IPostReducer {
