37145a331ae7c2bc2378fb45625962083c5b0581
ohjunsang <pandati@dsm.hs.kr>
2021-01-07 09:42:56 +0900

    [ADD] add post content

--- src/domain/entity/post/PostEntity.ts
+++ src/domain/entity/post/PostEntity.ts
@@ -1,16 +1,24 @@
-import IPostEntity from './interface/IPostEntity';
+import IPostEntity, { IContent } from './interface/IPostEntity';
 
 class PostEntity implements IPostEntity {
-  constructor(title: string, description: string, tags: string[], isResolved: boolean) {
+  constructor(
+    title: string,
+    description: string,
+    tags: string[],
+    content: IContent[],
+    isResolved: boolean,
+  ) {
     this.title = title;
     this.description = description;
     this.tags = tags;
     this.isResolved = isResolved;
+    this.content = content;
   }
   title: string;
   description: string;
   tags: string[];
   isResolved: boolean;
+  content: IContent[];
 }
 
 export default PostEntity;
