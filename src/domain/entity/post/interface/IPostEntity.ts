6c3bd53fdff23003481324d3ea407ba85e23778a
ohjunsang <pandati@dsm.hs.kr>
2021-01-07 09:42:42 +0900

    [ADD] post content interface

--- src/domain/entity/post/interface/IPostEntity.ts
+++ src/domain/entity/post/interface/IPostEntity.ts
@@ -1,8 +1,14 @@
+export interface IContent {
+  type: string;
+  content: string | string[];
+}
+
 interface IPostEntity {
   title: string;
   description: string;
   tags: string[];
   isResolved: boolean;
+  content: IContent[];
 }
 
 export default IPostEntity;
