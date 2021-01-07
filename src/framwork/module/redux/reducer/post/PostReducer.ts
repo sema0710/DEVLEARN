86f0483e3e68964f7439aafd042080153b957998
ohjunsang <pandati@dsm.hs.kr>
2021-01-07 09:51:50 +0900

    [FIX] post reducer

--- src/framwork/module/redux/reducer/post/PostReducer.ts
+++ src/framwork/module/redux/reducer/post/PostReducer.ts
@@ -1,69 +1,45 @@
 import { GET_MY_POSTS, GET_POSTS, SET_MY_POSTS, SET_POSTS } from '../../action/post/interface';
 import PostEntity from '../../../../../domain/entity/post';
 import { IPostReducer, IPostState } from './interface';
+import { GET_DETAIL_POST, IPostAction } from '../../action/post/interface/IPostAction';
 
 const initState: IPostState = {
   post: [
     new PostEntity(
       '테스트 글',
-      'hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, ,hello world',
+      'test',
       ['#테스트'],
+      [{ type: 'text', content: '테스트 글' }],
       false,
     ),
   ],
   mypost: [
     new PostEntity(
       '테스트 글',
-      'hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, ,hello world',
+      'test',
       ['#테스트'],
-      false,
-    ),
-    new PostEntity(
-      '테스트 글',
-      'hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, ,hello world',
-      ['#테스트'],
-      false,
-    ),
-    new PostEntity(
-      '테스트 글',
-      'hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, ,hello world',
-      ['#테스트'],
-      false,
-    ),
-    new PostEntity(
-      '테스트 글',
-      'hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, ,hello world',
-      ['#테스트'],
-      false,
-    ),
-    new PostEntity(
-      '테스트 글',
-      'hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, ,hello world',
-      ['#테스트'],
-      false,
-    ),
-    new PostEntity(
-      '테스트 글',
-      'hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, ,hello world',
-      ['#테스트'],
-      false,
-    ),
-    new PostEntity(
-      '테스트 글',
-      'hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, ,hello world',
-      ['#테스트'],
-      false,
-    ),
-    new PostEntity(
-      '테스트 글',
-      'hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, hello world, ,hello world',
-      ['#테스트'],
+      [{ type: 'text', content: '테스트 글' }],
       false,
     ),
   ],
+  detailPost: new PostEntity(
+    '테스트 글',
+    'test',
+    ['#테스트'],
+    [
+      { type: 'code', content: 'import test' },
+      { type: 'text', content: '테스트 글1' },
+      { type: 'text', content: '테스트 글3' },
+      { type: 'list', content: ['테스트 글4', '테스트 글5'] },
+    ],
+    false,
+  ),
+  answer: [
+    new PostEntity('테스트 글', 'test', [], [{ type: 'text', content: '테스트 글' }], false),
+  ],
 };
 
-const PostReducer: IPostReducer = (state = initState, action) => {
+const PostReducer: IPostReducer = (state: IPostState = initState, action: IPostAction) => {
   switch (action.type) {
     case GET_POSTS: {
       return state;
@@ -82,6 +58,9 @@ const PostReducer: IPostReducer = (state = initState, action) => {
         ...state,
         mypost: action.payload,
       };
+    }
+    case GET_DETAIL_POST: {
+      return state;
     }
     default: {
       return state;
